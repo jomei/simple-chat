a3ccd7b21f984ef68580802746c5875524f9fdac
anatolynosov <jomeisama@gmail.com>
2016-03-24 18:29:05 +0300

    Hello world

--- src/simple_chat/handler.clj
+++ src/simple_chat/handler.clj
@@ -1,6 +1,7 @@
 (ns simple-chat.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [org.httpkit.server :as httpkit]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
@@ -9,3 +10,7 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn start []
+  (def stop
+    (httpkit/run-server #'app {:port 3000})))
